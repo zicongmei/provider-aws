cedf3d463537f99a116357ace8e119a8e67d4fda
Zicong Mei <zicong@google.com>
2023-04-19 15:56:14 -0700

    rm files

--- apis/v1beta1/doc.go
+++
@@ -1,9 +0,0 @@
-/*
-Copyright 2022 Upbound Inc.
-*/
-
-// Package v1beta1 contains the core resources of the aws jet provider.
-// +kubebuilder:object:generate=true
-// +groupName=aws.upbound.io
-// +versionName=v1beta1
-package v1beta1
