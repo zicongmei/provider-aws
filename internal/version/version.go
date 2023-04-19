0a174481687a64263506b7f172a8d1f9cd55bd68
Zicong Mei <zicong@google.com>
2023-04-19 16:01:09 -0700

    reduce

--- internal/version/version.go
+++
@@ -1,9 +0,0 @@
-/*
-Copyright 2022 Upbound Inc.
-*/
-
-// Package version contains the version of provider-aws repo
-package version
-
-// Version will be overridden with the current version at build time using the -X linker flag
-var Version = "0.0.0"
